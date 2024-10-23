fb1718c6143f0c7c1a9c4ea595da1fc1d6e2c9a3
Resonatur <resonatur.144@gmail.com>
2024-10-22 20:53:57 -0500

    Initial commit for Buckshot Probability

---
+++ BuckshotProbability/BuckshotProbability/Program.cs
@@ -0,0 +1,16 @@
+﻿// Copyright Anri Amane ©
+
+using BuckshotProbability;
+
+public class Program
+{
+    /// <summary>
+    /// The entry point for the application.
+    /// </summary>
+    /// <param name="args"></param>
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+        ShellLoadPrinter 
+    }
+}
---
+++ BuckshotProbability/BuckshotProbability/ShellLoadPrinter.cs
@@ -0,0 +1,19 @@
+﻿// Copyright Anri Amane ©
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BuckshotProbability
+{
+    internal class ShellLoadPrinter
+    {
+        private static char BlankShell = '▯';
+
+        private static char LiveShell = '▮';
+
+
+    }
+}
